71699494ec115ca11073b2169af4063668a1f3a8
Cody06 <cody.miu@gmail.com>
2021-12-17 16:40:36 -0500

    Restyled landing page

--- landing_page/static/landing_page/js/index.js
+++ landing_page/static/landing_page/js/index.js
@@ -41,20 +41,20 @@ function show_resume() {
 	document.querySelector('#dropdown-content').style.display = 'none';
 	console.log("Show resume");
 	/* Show resume and blur background */
-	const pdf_container = document.querySelector('#pdf-container');
+	const pdf_modal = document.querySelector('#pdf-modal');
 	const main_container = document.querySelector('#main-container');
-	if (pdf_container.style.display === 'none') {
-		pdf_container.style.display = 'block';
+	if (pdf_modal.style.display === 'none') {
+		pdf_modal.style.display = 'block';
 		main_container.style.filter = 'blur(8px)';
 		console.log('show PDF')
 	}
-	else if (pdf_container.style.display === "") {	// Initially the style of the PDF container is an empty string
-		pdf_container.style.display = 'block'
+	else if (pdf_modal.style.display === "") {	// Initially the style of the PDF container is an empty string
+		pdf_modal.style.display = 'block'
 		main_container.style.filter = 'blur(8px)';
 		console.log('empty string style - show PDF')
 	}
 	else {
-		pdf_container.style.display = 'none';
+		pdf_modal.style.display = 'none';
 		main_container.style.filter = 'blur(0px)';
 		console.log('hide PDF')
 	}
@@ -64,13 +64,13 @@ function show_resume() {
 function close_resume() {
 	window.onclick = function(event) {
 		console.log(event.target)
-		const pdf_container = document.querySelector('#pdf-container');
-		// clicking on the pdf-container doesn't return anything (if true, we clicked outside)
+		const pdf_modal = document.querySelector('#pdf-modal');
+		// clicking on the pdf-modal doesn't return anything (if true, we clicked outside)
 		// clicking on any of the matches is not considered clicking outside the pdf container
 		if ( event.target && !event.target.matches('#resume-btn') && !event.target.matches('#projects-btn') 
 			&& !event.target.matches('#mobile-resume-btn') && !event.target.matches('#contact-btn') ) {	
-			console.log('Clicked outside pdf-container');
-			document.querySelector('#pdf-container').style.display = 'none';
+			console.log('Clicked outside pdf-modal');
+			document.querySelector('#pdf-modal').style.display = 'none';
 			document.querySelector('#main-container').style.filter = 'blur(0px)';
 		}
 	}
