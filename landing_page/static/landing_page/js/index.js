86c9f44d6efc5c59c17caf4fb88a7e7032b6a42b
Cody06 <cody.miu@gmail.com>
2022-09-04 21:18:31 -0400

    Variable name refactor

--- landing_page/static/landing_page/js/index.js
+++ landing_page/static/landing_page/js/index.js
@@ -1,34 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
 	document.querySelector('#resume-btn').addEventListener('click', show_resume);
 
-	const DROPDOWN_BTN = document.querySelector('#dropdown-btn');
-	if (DROPDOWN_BTN !== null) { DROPDOWN_BTN.addEventListener('click', show_dropdown); }
+	const dropdownButton = document.querySelector('#dropdown-btn');
+	if (dropdownButton !== null) { dropdownButton.addEventListener('click', show_dropdown); }
 	close_resume();
 
-	const PROJECTS_BTN = document.querySelector('#projects-btn');
-	if (PROJECTS_BTN !== null) { PROJECTS_BTN.addEventListener('click', scroll_to_projects); }
+	const projectButton = document.querySelector('#projects-btn');
+	if (projectButton !== null) { projectButton.addEventListener('click', scroll_to_projects); }
 
-	const MOBILE_PROJECTS_BTN = document.querySelector('#mobile-projects-btn');
-	if (MOBILE_PROJECTS_BTN !== null) { MOBILE_PROJECTS_BTN.addEventListener('click', scroll_to_projects); }
+	const mobileProjectButton = document.querySelector('#mobile-projects-btn');
+	if (mobileProjectButton !== null) { mobileProjectButton.addEventListener('click', scroll_to_projects); }
 
-	const MOBILE_RESUME_BTN = document.querySelector('#mobile-resume-btn');
-	if (MOBILE_RESUME_BTN !== null) { MOBILE_RESUME_BTN.addEventListener('click', show_resume); }
+	const mobileResumeButton = document.querySelector('#mobile-resume-btn');
+	if (mobileResumeButton !== null) { mobileResumeButton.addEventListener('click', show_resume); }
 
-	const CONTACT_BTN = document.querySelector('#contact-btn');
-	if (CONTACT_BTN !== null) { CONTACT_BTN.addEventListener('click', scroll_to_contact); }
+	const contactButton = document.querySelector('#contact-btn');
+	if (contactButton !== null) { contactButton.addEventListener('click', scroll_to_contact); }
 
-	const MOBILE_CONTACT_BTN = document.querySelector('#mobile-contact-btn');
-	if (MOBILE_CONTACT_BTN !== null) { MOBILE_CONTACT_BTN.addEventListener('click', scroll_to_contact); }
+	const mobileContactButton = document.querySelector('#mobile-contact-btn');
+	if (mobileContactButton !== null) { mobileContactButton.addEventListener('click', scroll_to_contact); }
 });
 
 function show_dropdown() {
-	const DROPDOWN_CONTENT = document.querySelector('#dropdown-content');
+	const dropdownContent = document.querySelector('#dropdown-content');
 
-	if (DROPDOWN_CONTENT.style.display == 'none' || DROPDOWN_CONTENT.style.display == "") {
-		DROPDOWN_CONTENT.style.display = 'block';
+	if (dropdownContent.style.display == 'none' || dropdownContent.style.display == "") {
+		dropdownContent.style.display = 'block';
 	}
 	else {
-		DROPDOWN_CONTENT.style.display = 'none';
+		dropdownContent.style.display = 'none';
 	}
 }
 
@@ -44,33 +44,30 @@ function scroll_to_contact() {
 
 function show_resume() {
 	document.querySelector('#dropdown-content').style.display = 'none';
-	console.log("Show resume");
+
 	/* Show resume and blur background */
-	const pdf_modal = document.querySelector('#pdf-modal');
+	const pdfModal = document.querySelector('#pdf-modal');
 	const main_container = document.querySelector('#main-container');
-	if (pdf_modal.style.display === 'none') {
-		pdf_modal.style.display = 'block';
+	if (pdfModal.style.display === 'none') {
+		pdfModal.style.display = 'block';
 		document.querySelector('#darken-overlay').style.display = 'block';
 		main_container.style.filter = 'blur(8px)';
 
 		prevent_scroll();
-		console.log('show PDF');
 	}
-	else if (pdf_modal.style.display === "") {	// Initially the style of the PDF container is an empty string
-		pdf_modal.style.display = 'block'
+	else if (pdfModal.style.display === "") {	// Initially the style of the PDF container is an empty string
+		pdfModal.style.display = 'block'
 		document.querySelector('#darken-overlay').style.display = 'block';
 		main_container.style.filter = 'blur(8px)';
 		
 		prevent_scroll();
-		console.log('empty string style - show PDF')
 	}
 	else {
-		pdf_modal.style.display = 'none';
+		pdfModal.style.display = 'none';
 		document.querySelector('#darken-overlay').style.display = 'none';
 		main_container.style.filter = 'blur(0px)';
 
 		resume_scroll();
-		console.log('hide PDF');
 	}
 }
 
@@ -78,13 +75,11 @@ function show_resume() {
 function close_resume() {
 	// Close resume display if we clicked outside its container
 	window.onclick = function(event) {
-		console.log(event.target);
-		const pdf_modal = document.querySelector('#pdf-modal');
+		const pdfModal = document.querySelector('#pdf-modal');
 		// clicking on the pdf-modal doesn't return anything (if true, we clicked outside)
 		// clicking on any of the matches is not considered clicking outside the pdf container
 		if ( event.target && !event.target.matches('#resume-btn') && !event.target.matches('#projects-btn') 
 			&& !event.target.matches('#mobile-resume-btn') && !event.target.matches('#contact-btn') ) {	
-			console.log('Clicked outside pdf-modal');
 			document.querySelector('#pdf-modal').style.display = 'none';
 			document.querySelector('#darken-overlay').style.display = 'none';
 			document.querySelector('#main-container').style.filter = 'blur(0px)';
